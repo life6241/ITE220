0ea60db7edfd196b40ca3ffbaa0840ada7806e3e
life6241 <sebastian.sukler@students.stamford.edu>
2016-04-01 01:37:06 +0700

    Submission for W2D2 Assignment#2 (Stamford Shopping Center Website)
    Week2/index.html
    - Added Greeting for customer that is based on local time
    - Added Discount price total
    - Added Text display for discount price
    
    Week2/js/shop.js
    - Added if else statements
    -Discount code
    
    /index.html
    -Added css lines for homework links

--- Week2/js/shop.js
+++ Week2/js/shop.js
@@ -1,10 +1,46 @@
 var customerName = "Sebastian";
 var price = 10;
-var quantity = 2;
-var totalPrice = price * quantity;
+
+var time = new Date().getHours();
+var greeting;
+if (time < 12) {
+	greeting = "Good Moring";
+}
+	else if (time < 23){
+		greeting = "Good Afternoon";
+	}
+	else {
+		greeting = "Good Evening";
+	}
+var greetingElement = document.getElementById("greeting-time");
+greetingElement.textContent = greeting;
 
 var customerElement = document.getElementById("customer-name");
 customerElement.textContent = customerName;
 
 var priceElement = document.getElementById("total-price");
 priceElement.textContent = totalPrice;
+
+var products = [
+	"Brooklyn T-Shirt White",
+	"Brooklyn T-Shirt Black",
+	"Apple Watch",
+	"Android Phone"
+	];
+var prices = [10, 10, 199, 159,];
+
+var productsText = "";
+var productsElement = document.getElementById("product-list");
+
+productsText += "<li class='list-group-item'><span class='badge'>$"+ prices[0]+"</span>" + products[0]+ "</li>";
+productsText += "<li class='list-group-item'><span class='badge'>$"+ prices[1]+"</span>" + products[1]+ "</li>";
+productsText += "<li class='list-group-item'><span class='badge'>$"+ prices[2]+"</span>" + products[2]+ "</li>";
+productsText += "<li class='list-group-item'><span class='badge'>$"+ prices[3]+"</span>" + products[3]+ "</li>";
+productsElement.innerHTML = productsText;
+
+var price = 0;
+var discount = 0.75;
+price = prices[0] +prices[1] +prices[2] +prices[3];
+var totalPrice = price * discount;
+var totalPriceElement = document.getElementById("total-price");  
+totalPriceElement.textContent = totalPrice;
